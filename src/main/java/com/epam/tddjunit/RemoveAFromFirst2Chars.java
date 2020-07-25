77aae450b89bae7b4cbc4f6ca318993d7122b191
Thadisina Pravallika Reddy <62056126+PravallikaThadisina@users.noreply.github.com>
2020-07-25 20:30:58 +0530

    Add files via upload

---
+++ src/main/java/com/epam/tddjunit/RemoveAFromFirst2Chars.java
@@ -0,0 +1,21 @@
+package com.epam.tddjunit;
+
+public class RemoveAFromFirst2Chars {
+	public String remove(String string) {
+		int length=string.length();
+		String result="";
+		if(length>0 && string.charAt(0)!='A') result+=string.charAt(0);
+		if(length>1)
+		{
+			if(string.charAt(1)!='A') result+=string.charAt(1);
+		}
+		if(length>2)
+		{
+			for(int i=2;i<length;i++) result+=string.charAt(i);
+		}
+		return result;
+	}
+     
+
+
+}
